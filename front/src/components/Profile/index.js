0f309fac0a9ece31f792aaef08a179ab78e18305
Eros003 <corentin.dc.eros@gmail.com>
2020-07-27 14:54:15 +0200

    add login middleware

---
+++ front/src/components/Profile/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const Profile = () => (
+  <div>
+    <ul className="categories__ul">
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+      <li className="categories__item">
+        <h3>Catégorie </h3>
+        <p>Progression</p>
+        <progress id="progression" max="100" value="70"> 70% </progress>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Profile;
---
+++ front/src/middlewares/login.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { LOGIN, changeFieldValue } from 'src/actions';
+
+const login = (store) => (next) => (action) => {
+  switch (action.type) {
+    case LOGIN: {
+      const { email, password } = store.getState();
+
+      axios.post('/*', {
+        email,
+        password,
+      })
+        .then((response) => {
+          console.log(response.data);
+          store.dispatch(changeFieldValue('username', response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      next(action);
+      break;
+    }
+    default:
+      next(action);
+  }
+};
+
+export default login;
