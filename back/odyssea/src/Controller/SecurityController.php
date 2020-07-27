f9c260d6e2a971ff5f6e5cf576110a7f2c50cbb1
audreygmrd <audrey.guimard@kedgebs.com>
2020-07-27 16:54:55 +0200

    tests login

--- back/odyssea/src/Controller/SecurityController.php
+++ back/odyssea/src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,18 +17,45 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, SerializerInterface $serializer)
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // Le JSON est dans le contenu de la requête
+        $content = $request->getContent();
+
+        // On déserialise notre JSON en entité Doctrine
+        $user = $serializer->deserialize($content, User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $user]);
+
+        return $this->redirectToRoute('api_login',[
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/api/login", name="api_login")
+     */
+    public function ApiLogin(AuthenticationUtils $authenticationUtils, UserRepository $userRepository, Request $request, SerializerInterface $serializer)
+    {
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // Le JSON est dans le contenu de la requête
+        $content = $request->getContent();
+
+        $currentUser = $userRepository->find($user->getId());
+        
+        // On déserialise notre JSON en entité Doctrine
+        $user = $serializer->deserialize($content, User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+
+        //dd($currentUser);
+
+        return $this->json([
+            'last_username' => $lastUsername, 
+            'user' => $user,
+            'error' => $error
+        ]);
     }
 
     /**
--- back/odyssea/src/Security/LoginAuthenticator.php
+++ back/odyssea/src/Security/LoginAuthenticator.php
@@ -96,8 +96,8 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('api_login'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
