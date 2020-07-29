80eb654c8a950ab263e7be993f6f8e403c97881e
annebequet <a.bequet@live.fr>
2020-07-29 10:29:17 +0200

    fix middleware reducer register

--- front/src/middlewares/index.js
+++ front/src/middlewares/index.js
@@ -1,9 +1,11 @@
 import logMiddleware from './logMiddleware';
 import login from './login';
 import surveys from './surveys';
+import register from './register';
 
 export default [
   surveys,
   logMiddleware,
   login,
+  register,
 ];
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import headerLogin from './headerLogin';
 import surveys from './surveys';
+import register from './register';
 
 export default combineReducers({
   headerLogin,
+  register,
   surveys,
 });
