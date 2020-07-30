d192ad2aeb78265f29a996ede47029b511fc7859
Eros003 <corentin.dc.eros@gmail.com>
2020-07-30 11:12:46 +0200

    better register css

--- front/src/actions/register.js
+++ front/src/actions/register.js
@@ -1,5 +1,6 @@
 export const CHANGE_FIELD = 'CHANGE_FIELD';
 export const REGISTER = 'REGISTER';
+export const HANDLE_SELECT = 'HANDLE_SELECT';
 
 export const changeField = (value, name) => ({
   type: CHANGE_FIELD,
