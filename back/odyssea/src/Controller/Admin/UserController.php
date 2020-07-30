e8e85b0da5028ca4fcd1e59c836bd729240f400e
audreygmrd <audrey.guimard@kedgebs.com>
2020-07-30 16:00:35 +0200

    (pending) backoffice User

--- back/odyssea/src/Controller/Admin/UserController.php
+++ back/odyssea/src/Controller/Admin/UserController.php
@@ -2,18 +2,85 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\Admin\UserType;
+use Psr\Log\LoggerInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * @Route("admin/user")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/admin/user", name="admin_user")
+     * @Route("/browse", name="admin_user_browse", methods={"GET"})
      */
-    public function index()
+    public function browse(UserRepository $userRepository): Response
     {
-        return $this->render('admin/user/index.html.twig', [
-            'controller_name' => 'UserController',
+        return $this->render('admin/user/browse.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/read/{id}", name="admin_user_read", methods={"GET"})
+     */
+    public function read(User $user): Response
+    {
+        return $this->render('admin/user/read.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="admin_user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Editer'
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $form->get('password')->getData();
+
+
+            if (! empty($password))
+            {
+                // le hasher
+                $passwordHashed = $passwordEncoder->encodePassword($user, $password);
+                // le mettre dans l'entité User avant d'enregistrer en BDD
+                $user->setPassword($passwordHashed);
+            }
+            
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_user_browse');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="admin_user_delete", methods={"DELETE"})
+     */
+    public function delete(User $user): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_user_browse');
     }
 }
