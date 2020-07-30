72e1ad8c4602dd8910f0b654924388b4c46a83fe
audreygmrd <audrey.guimard@kedgebs.com>
2020-07-30 22:35:18 +0200

    (pending) css templates admin user

--- back/odyssea/src/Controller/Admin/UserController.php
+++ back/odyssea/src/Controller/Admin/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -52,15 +53,14 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $password = $form->get('password')->getData();
 
-
-            if (! empty($password))
+            if (!empty($password))
             {
                 // le hasher
                 $passwordHashed = $passwordEncoder->encodePassword($user, $password);
                 // le mettre dans l'entité User avant d'enregistrer en BDD
                 $user->setPassword($passwordHashed);
             }
-            
+            $user->setUpdatedAt(new \DateTime());
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('admin_user_browse');
@@ -73,9 +73,42 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/delete/{id}", name="admin_user_delete", methods={"DELETE"})
+     * @Route("/add", name="admin_user_add", methods={"GET","POST"})
      */
-    public function delete(User $user): Response
+    public function add(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Ajouter'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // hasher le pwd reçu
+            $passwordHashed = $passwordEncoder->encodePassword($user, $user->getPassword());
+            // le mettre dans l'entité User avant d'enregistrer en BDD
+            $user->setPassword($passwordHashed);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_user_browse');
+        }
+
+        return $this->render('admin/user/add.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="admin_user_delete", methods={"GET"})
+     */
+    public function delete(Request $request, User $user): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($user);
--- back/odyssea/src/Form/Admin/UserType.php
+++ back/odyssea/src/Form/Admin/UserType.php
@@ -3,33 +3,76 @@
 namespace App\Form\Admin;
 
 use App\Entity\User;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('lastName')
             ->add('firstName')
+            ->add('lastName')
             ->add('pseudo')
-            ->add('roles')
-            ->add('password')
-            ->add('avatar')
-            ->add('apiToken')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('environment')
-        ;
+            ->add('email', null, [
+                'empty_data' => '',
+                ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                // On récupère l'entité User
+                $user = $event->getData();
+                // On récupère le builder pour continuer le form
+                $builder = $event->getForm();
+
+                // Si notre user est existant en database, on applique le mapped=false
+                // S'il a un id, il existe en base
+                if ($user->getId() !== null) {
+                    $builder->add('password', PasswordType::class, [
+                        'empty_data' => '',
+                        'mapped' => false,
+                        'attr' => [
+                            'placeholder' => 'Laisser vide si inchangé',
+                        ]
+                    ]);
+                } else {
+                    $builder->add('password', PasswordType::class, [
+                        'empty_data' => '',
+                    ]);
+                }
+            })
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER'
+                ],
+                'expanded' => true,
+                'multiple' => true
+            ])
+            ->add('avatar', ChoiceType::class, [
+                'choices' => [
+                    'Crabe' => 'https://image.flaticon.com/icons/svg/1805/1805801.svg',
+                    'Dauphin' => 'https://image.flaticon.com/icons/svg/1805/1805946.svg',
+                    'Phoque' => 'https://image.flaticon.com/icons/svg/1805/1805880.svg',
+                    'Tortue' => 'https://image.flaticon.com/icons/svg/1805/1805958.svg'
+                ],
+                'expanded' => true,
+                'multiple' => false
+            ])
+            ->add('environment', null, [
+                'label' => 'Environnement',
+                'placeholder' => 'Choisissez un environnement'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'attr' => ['novalidate' => 'novalidate']
         ]);
     }
 }
