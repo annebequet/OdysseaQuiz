f02d2462eb441571cae587c4469c61c69fc84deb
Eros003 <corentin.dc.eros@gmail.com>
2020-07-31 14:59:13 +0200

    (new/add)routes/categories

--- front/src/components/Categories/index.js
+++ front/src/components/Categories/index.js
@@ -4,65 +4,27 @@ import TurtleImage from 'src/assets/images/turtle.jpg';
 
 import './styles.scss';
 
-const Categories = () => (
+const Categories = ({ categories }) => (
   <div className="page__categories">
     <ul className="categories__ul">
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
-      <li className="categories__item">
-        <h3>Catégorie </h3>
-        <img
-          src={TurtleImage}
-        />
-      </li>
+      {categories.map(({ name }) => (
+        <li className="categories__item">
+          <h3>{name} </h3>
+          <img
+            src={TurtleImage}
+          />
+        </li>
+      ))}
     </ul>
   </div>
 );
+
+Categories.propTypes = {
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default Categories;
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import headerLogin from './headerLogin';
 import surveys from './surveys';
 import register from './register';
+import categories from './categories';
 
 export default combineReducers({
   headerLogin,
   register,
   surveys,
+  categories,
 });
