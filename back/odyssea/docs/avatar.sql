9dcc3c0f826dc41dad7d48cff3885c466e4156f1
audreygmrd <audrey.guimard@kedgebs.com>
2020-08-03 18:01:58 +0200

    (fix) database (ls w/ Clemence)

---
+++ back/odyssea/docs/avatar.sql
@@ -0,0 +1,10 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `gallery` (`id`, `image_url`, `created_at`, `updated_at`) VALUES
+(1,	'https://image.flaticon.com/icons/svg/1805/1805958.svg',	'2020-08-03 17:43:20',	NULL),
+(2,	'https://image.flaticon.com/icons/svg/1805/1805880.svg',	'2020-08-03 17:43:38',	NULL),
+(3,	'https://image.flaticon.com/icons/svg/1805/1805946.svg',	'2020-08-03 17:44:03',	NULL),
+(4,	'https://image.flaticon.com/icons/svg/1805/1805808.svg',	'2020-08-03 17:44:31',	NULL),
+(5,	'https://image.flaticon.com/icons/svg/1805/1805804.svg',	'2020-08-03 17:45:53',	NULL),
+(6,	'https://image.flaticon.com/icons/svg/1805/1805814.svg',	'2020-08-03 17:46:24',	NULL),
+(7,	'https://image.flaticon.com/icons/svg/1805/1805927.svg',	'2020-08-03 17:46:43',	NULL);
--- back/odyssea/src/Entity/Gallery.php
+++ back/odyssea/src/Entity/Gallery.php
@@ -72,4 +72,9 @@ class Gallery
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->imageUrl;
+    }
 }
