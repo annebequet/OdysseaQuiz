79bbf72fd040f3c46a0c1f2f0381bedbea3cc397
audreygmrd <audrey.guimard@kedgebs.com>
2020-08-04 17:33:21 +0200

    (build) Question crud controller

--- back/odyssea/src/Controller/Admin/QuestionCrudController.php
+++ back/odyssea/src/Controller/Admin/QuestionCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class QuestionCrudController extends AbstractCrudController
 {
@@ -12,14 +18,19 @@ class QuestionCrudController extends AbstractCrudController
         return Question::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
+            TextField::new('type'),
+            TextField::new('name'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            ArrayField::new('choices'),
+            TextField::new('correctAnswer'),
+            AssociationField::new('category'),
+            AssociationField::new('environment')
         ];
     }
-    */
+    
 }
