9b8fc1ff63a2bba4cd4ff524866c641c4270b596
audreygmrd <audrey.guimard@kedgebs.com>
2020-08-04 17:50:46 +0200

    (fix) subscriber

--- back/odyssea/src/Controller/Admin/QuestionCrudController.php
+++ back/odyssea/src/Controller/Admin/QuestionCrudController.php
@@ -8,7 +8,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class QuestionCrudController extends AbstractCrudController
@@ -22,14 +21,18 @@ class QuestionCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('type'),
-            TextField::new('name'),
-            TextField::new('title'),
-            ArrayField::new('choices'),
-            TextField::new('correctAnswer'),
-            AssociationField::new('category'),
-            AssociationField::new('environment')
+            IdField::new('id')
+                ->onlyOnIndex(),
+            TextField::new('type')
+                ->hideOnIndex(),
+            TextField::new('name', 'Slug')
+                ->hideOnIndex(),
+            TextareaField::new('title', 'Question'),
+            ArrayField::new('choices', 'Réponses proposées')
+                ->setHelp('Entrez 4 réponses seulement'),
+            TextField::new('correctAnswer', 'Bonne réponse'),
+            AssociationField::new('category', 'Catégorie'),
+            AssociationField::new('environment', 'Environnement')
         ];
     }
     
