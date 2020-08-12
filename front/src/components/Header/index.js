8916a6137ae41726fed243d2717f5714493e401b
audreygmrd <audrey.guimard@kedgebs.com>
2020-08-12 12:41:54 +0200

    (fix) link admin header

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -36,7 +36,7 @@ const Header = ({ roles, isLogged, loginAdmin }) => {
         </NavLink>
         {(roles.indexOf('ROLE_ADMIN') !== -1) && (
         <a
-          href="http://localhost/Apotheose/Odyssea/back/odyssea/public/admin/login"
+          href="http://54.226.34.31/back/admin/login"
           className="menu__item"
           onClick={handleAdmin}
         >
