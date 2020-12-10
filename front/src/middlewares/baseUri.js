0acfeb074a4c1679e4afe79293c96c1634ca8fed
audreygmrd <audrey.guimard@kedgebs.com>
2020-12-10 13:42:59 +0100

    ready for deployment

--- front/src/middlewares/baseUri.js
+++ front/src/middlewares/baseUri.js
@@ -1,5 +1,7 @@
 // Url localhost Anne : 'http://localhost/Anne/OdysseaQuiz/back/odyssea/public/api'
+// Url localhost Audrey : http://localhost/tp/OdysseaQuiz/back/odyssea/public/api'
+// Url AWS : 'http://54.237.18.247/api'
 
-const baseUrl = 'http://localhost/Anne/OdysseaQuiz/back/odyssea/public/api';
+const baseUrl = 'http://54.237.18.247/api';
 
 export default baseUrl;
