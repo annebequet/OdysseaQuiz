af4a391031322d45fdc615a0f1df6d60b6818ee8
annebequet <a.bequet@live.fr>
2020-12-13 18:44:19 +0100

    build

--- front/src/data/index.js
+++ front/src/data/index.js
@@ -131,6 +131,204 @@ export const quizChildrenExemple = [
     ],
     correctAnswer: 'lion',
   },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
+  {
+    type: 'imagepicker',
+    id: 1,
+    name: 'choosepicture',
+    title: 'What animal would you like to see first ?',
+    choices: [
+      {
+        value: 'lion',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg',
+      }, {
+        value: 'giraffe',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg',
+      }, {
+        value: 'panda',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg',
+      }, {
+        value: 'camel',
+        imageLink: 'https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg',
+      },
+    ],
+    correctAnswer: 'lion',
+  },
 ];
 
 { /* Exemple of how to insert images in the quiz, thanks to markdown:
